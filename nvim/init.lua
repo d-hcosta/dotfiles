5a9da96ca34d779819e68e38b184b19a78b62bd7
d-hcosta <d.hcosta@proton.me>
2025-03-05 04:05:18 -0300

    Adding new nvim config files

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+-- Load the options from the config/options.lua file
+require("config.options")
+-- Load the keymaps from the config/keymaps.lua file
+require("config.keymaps")
+-- Setup lazy, this should always be last
+-- Tell lazy that all plugin specs are found in the plugins directory
+-- Pass it the options we specified above
+require("config.lazy")
